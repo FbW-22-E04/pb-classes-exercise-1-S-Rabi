58bd2b426222a904eb37e409b3ed5c375313418d
S Rabi <rabi.sanaye@yahoo.com>
2022-08-25 07:28:31 +0200

    second

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.fullname = `${firstname} ${lastname}`;
+    this.email = `${firstname}.${lastname}@company.com`.toLowerCase();
+  }
 }
+emp1 = new Employee("John", "Smith");
+emp2 = new Employee("Mary", "Sue");
+emp3 = new Employee("Antony", "Walker");
+
+console.log(emp1.fullname);
+console.log(emp2.email);
+console.log(emp3.firstname);
